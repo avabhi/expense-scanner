1b7169cbf90c2650c7ec7520bc0446fd767fcfe4
Abhinav Vishwakarma <vabhinav368@gmail.com>
2026-06-13 00:37:16 +0530

    refactor: remove redundant id and name fields and add generic initializer to Base class

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm import as_declarative
 
 @as_declarative()
 class Base:
-    id: Any
-    __name__: str
-
     # Generate __tablename__ automatically
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
+
+    def __init__(self, **kwargs: Any) -> None:
+        for k, v in kwargs.items():
+            setattr(self, k, v)
